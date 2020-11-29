660ea14e4a0f17bab06bcd4cb25d45352d2521aa
Miorey <Clem.Balea@GMail.COM>
2020-11-29 16:59:32 +0100

    add test and eslint

---
+++ test/trinitycore-srp6.test.js
@@ -0,0 +1,16 @@
+/*jshint esversion: 6 */
+/* eslint-env node */
+"use strict";
+
+
+/**
+ * Created by clem on 4/4/17.
+ */
+'use strict';
+
+describe("Translator test", function() {
+
+    it("Empty Test", function() {
+    })
+
+})
