7d5eae04aae8ab258bc0c76f5f5ef789eaad0fcd
Miorey <clem.balea@gmail.com>
2021-09-17 01:10:05 +0300

    add test

--- test/trinitycore-srp6.test.js
+++ test/trinitycore-srp6.test.js
@@ -2,15 +2,21 @@
 /* eslint-env node */
 "use strict"
 
+const crypto = require(`crypto`)
+const { computeVerifier, params }  = require(`../index`)
 
 /**
  * Created by clem on 4/4/17.
  */
 'use strict'
 
-describe("Translator test", function() {
+describe("test verifier", function() {
 
-    it("Empty Test", function() {
+    it("Test verifier generate from salt", function() {
+        crypto.randomBytes(32, (err, buf) => {
+            if (err) throw err;
+            computeVerifier(params.trinitycore, buf, `tic`, `tac`);
+        });
     })
 
 })
