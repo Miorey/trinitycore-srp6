42d79bc10bb36b20c47e5ff86b58c78b6f9e3a29
clem <clem.balea@gmail.com>
2024-02-15 12:17:44 +0200

    add TS support

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,43 +1,42 @@
 /**
- *
- * @param {{}} params (obj) group parameters, with .N, .g, .hash
- * @param {Buffer} salt
- * @param {string} identity
- * @param {string} password
- * @return {Buffer}
- * The verifier is calculated as described in Section 3 of [SRP-RFC].
- * We give the algorithm here for convenience.
- *
- * The verifier (v) is little endian computed based on the salt (s), user name (I),
- * password (P), and group parameters (N, g).
- *
- *         x = LE(H(s | H(user | ":" | pass)))
- *         v = LE(g^x % N)
- *
- * params:
- *         params (obj)     group parameters, with .N, .g, .hash
- *         salt (buffer)        salt
- *         identity (string)    user identity
- *         identity (string)    user password
- *         LE                   little endian
- *
+ * Parameters for cryptographic operations.
  */
-export function computeVerifier(params: {}, salt: Buffer, identity: string, password: string): Buffer;
-export namespace params {
-    namespace trinitycore {
-        const N_length_bits: number;
-        const N: bigint;
-        const g: bigint;
-        const hash: string;
-    }
-    namespace azerothcore {
-        const N_length_bits_1: number;
-        export { N_length_bits_1 as N_length_bits };
-        const N_1: bigint;
-        export { N_1 as N };
-        const g_1: bigint;
-        export { g_1 as g };
-        const hash_1: string;
-        export { hash_1 as hash };
-    }
-}
+export type Params = {
+    /**
+     * - Length of N in bits.
+     */
+    N_length_bits: number;
+    /**
+     * - Large safe prime.
+     */
+    N: BigInt;
+    /**
+     * - Generator.
+     */
+    g: BigInt;
+    /**
+     * - Hash function.
+     */
+    hash: string;
+};
+/**
+ * Computes the verifier.
+ * @param {Params} params - Group parameters.
+ * @param {Buffer} salt - Salt.
+ * @param {string} identity - User identity.
+ * @param {string} password - User password.
+ * @returns {Buffer} - Computed verifier.
+ */
+export function computeVerifier(params: Params, salt: Buffer, identity: string, password: string): Buffer;
+/**
+ * Parameters for cryptographic operations.
+ * @typedef {Object} Params
+ * @property {number} N_length_bits - Length of N in bits.
+ * @property {BigInt} N - Large safe prime.
+ * @property {BigInt} g - Generator.
+ * @property {string} hash - Hash function.
+ */
+/** @type {Object<string, Params>} */
+export const params: {
+    [x: string]: Params;
+};
